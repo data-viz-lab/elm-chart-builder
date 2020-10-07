a43ff19d0e0f2728aa2e6a520000147fc1ea46d9
deciob <decio.battaglia@gmail.com>
2020-10-07 18:55:28 +0100

    saving wip on screenshot tests setup

--- screenshot-tests/tests.js
+++ screenshot-tests/tests.js
@@ -1,7 +1,12 @@
 const puppeteer = require("puppeteer");
-
 const fs = require("fs");
 const path = require("path");
+const PNG = require("pngjs").PNG;
+const pixelmatch = require("pixelmatch");
+
+const args = process.argv.slice(2);
+
+let problems = false;
 
 const getAllFiles = (dirPath, arrayOfFiles = []) => {
   files = fs.readdirSync(dirPath);
@@ -22,7 +27,35 @@ const getAllFiles = (dirPath, arrayOfFiles = []) => {
 
 const replacer = __dirname.replace("screenshot-tests", "examples/");
 const allFiles = getAllFiles("../examples").map((f) => f.replace(replacer, ""));
-console.log(allFiles);
+
+const compareImages = () => {
+  for (var i = 0, len = allFiles.length - 1; i < len; i++) {
+    const pngName = allFiles[i].replace(".elm", ".png").replace("/", "--");
+    const currentName = pngName.replace(".", "--current.");
+    const prevName = pngName.replace(".", "--prev.");
+    const diffName = pngName.replace(".", "--diff.");
+
+    if (fs.existsSync("images/" + prevName)) {
+      const current = PNG.sync.read(fs.readFileSync("images/" + currentName));
+      const prev = PNG.sync.read(fs.readFileSync("images/" + prevName));
+      const { width, height } = prev;
+      const diff = new PNG({ width, height });
+
+      const zz = pixelmatch(prev.data, current.data, diff.data, width, height, {
+        threshold: 0.1,
+      });
+
+      if (zz !== 0) {
+        console.warn(`Page ${allFiles[i]} has changed!`);
+        problems = true;
+      }
+    }
+  }
+
+  if (!problems) {
+    console.warn("All pages are good");
+  }
+};
 
 const buildScreenshots = async () => {
   const browser = await puppeteer.launch();
@@ -35,11 +68,27 @@ const buildScreenshots = async () => {
 
   for (var i = 0, len = allFiles.length - 1; i < len; i++) {
     const png = allFiles[i].replace(".elm", ".png").replace("/", "--");
+
+    const current = png.replace(".", "--current.");
+    const prev = png.replace(".", "--prev.");
+
+    if (
+      (fs.existsSync("images/" + current) && args[0] === "--reset") ||
+      (fs.existsSync("images/" + current) && !fs.existsSync("images/" + prev))
+    ) {
+      // rename the existing screenshots
+      fs.rename("images/" + current, "images/" + prev, (err) => {
+        if (err) console.log("ERROR: " + err);
+      });
+    }
+
     await page.goto("http://localhost:8000/" + allFiles[i]);
-    await page.screenshot({ path: "images/" + png });
+    await page.screenshot({ path: "images/" + current });
   }
 
   await browser.close();
+
+  await compareImages();
 };
 
 buildScreenshots();
